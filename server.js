f5acfaf6066824fd44d108b98429b170d2841b91
montechristiaan <christiaan.simmons@live.com>
2019-05-28 12:12:56 -0500

    added models

--- server.js
+++ server.js
@@ -18,16 +18,73 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/mongoscraper", { useNewUrlParser: true});
+mongoose.connect("mongodb://localhost/mongoscraper", { useNewUrlParser: true });
 
-app.get("/scrape", function(req, res) {
-    axios.get("https://pitchfork.com/").then(function(response) {
+app.get("/scrape", function (req, res) {
+    axios.get("https://pitchfork.com/").then(function (response) {
         var $ = cheerio.load(response.data);
 
-        $()
-    })
+        $("story text").each(function (i, element) {
+            var result = {};
 
-})
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .chilren("a")
+                .attr("href");
+
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        });
+        res.send("Scrape Complete");
+    });
+});
+
+app.get("/articles", function (req, res) {
+    db.Article.find({})
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
+
+app.get("/articles/:id", function (req, res) {
+    db.Article.findOne({ _id: req.params.id })
+        .populate("note")
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
+
+app.post("/articles/:id", function (req, res) {
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        })
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
+
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
+
+
 
 
 
